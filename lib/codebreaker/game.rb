1c477e743fbb6ded39c275ace5a3f74699615fca
mclegea <mclegea@gmail.com>
2015-06-18 23:00:17 +0300

    tabs_to_spaces

--- lib/codebreaker/game.rb
+++ lib/codebreaker/game.rb
@@ -7,46 +7,44 @@ module Codebreaker
     end
 
     def guess(player_code)
-    	@attempts -= 1  unless @attempts <= 0
-    	attempt_code = player_code.each_char.map{ |i| i.to_i }
-       	answer = {}
-    	
-    	#get "+"
-    	
-    	for i in 0..3
-    		if @secret_code.at(i) == attempt_code.at(i)
-    			answer[i] = "+"
-    		end
-     	end
-    	
-    	#get "-"
-    	
-    	secret = Hash[(0...@secret_code.size).zip(@secret_code)]
-    	attempt = Hash[(0...attempt_code.size).zip(attempt_code)]
+      @attempts -= 1  unless @attempts <= 0
+      attempt_code = player_code.each_char.map{ |i| i.to_i }
+      answer = {}
 
-    	vars = [attempt, secret]
+      #get "+"
+      for i in 0..3
+        if @secret_code.at(i) == attempt_code.at(i)
+          answer[i] = "+"
+        end
+      end
 
-    	vars.each do |var|
-    		var.delete_if do |key,value|
-    			key = answer[key]
-    		end
-    	end
+      #get "-"
+      secret = Hash[(0...@secret_code.size).zip(@secret_code)]
+      attempt = Hash[(0...attempt_code.size).zip(attempt_code)]
 
+      vars = [attempt, secret]
 
-    	attempt.each_value do |value|
-    		if secret.has_value?(value)
-    			i += 10
-    			answer[i] = "-"
-      		end
-    	end
-    	
-    	result = ""
-    	
-    	answer.each_value do |value|
-    		result << value
-    	end
+      vars.each do |var|
+        var.delete_if do |key,value|
+          key = answer[key]
+        end
+      end
 
-    	return result
+      attempt.each_value do |value|
+        if secret.has_value?(value)
+          i += 10
+          answer[i] = "-"
+        end
+      end
+      
+      result = ""
+
+      answer.each_value do |value|
+        result << value
+      end
+
+      return result
+
     end
 
     def hint
